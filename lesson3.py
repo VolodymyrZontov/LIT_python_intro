1d2f4d17b7bdd36d355742f982082fbf38a23848
Volodymyr Zontov <volodymyr.z@cybord.ai>
2024-10-06 15:21:14 +0200

    lesson3

---
+++ lesson3.py
@@ -0,0 +1,36 @@
+# value_1 = 12
+# value_2 = 1.2
+# value_3 = "12"
+#
+# print(value_1, value_2, value_3, type(value_1), type(value_2), type(value_3))
+
+###########################################################
+# str
+str_1 = "john DOW"
+# str_1 = "1234"
+# result = str_1 + "str_1"
+# print(result)
+
+# result_2 = 2 * str_1 * 10
+# print(result_2)
+# len_str_1 = len(str_1)
+# print(len_str_1)
+
+# sub_str_1 = str_1[1:5:2]  # Перший - включно, останній - не включно, третя позиція - крок зрізу
+# sub_str_1 = str_1[::-1]  # "розворот" строки
+# print(sub_str_1)
+
+# symbol = str_1[-1]  # Перший символ з кінця строки
+# print(symbol)
+# sub_str_1 = str_1[2:-1]
+# print(sub_str_1)
+
+###########################################################
+# Методи строк
+
+# str_1 = str_1.replace("asd", "ASD")
+# str_1 = str_1.upper()
+# str_1 = str_1.lower()
+str_1 = str_1.capitalize()
+print(str_1)
+
