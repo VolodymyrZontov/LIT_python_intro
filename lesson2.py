82a0f7c3d1b96b579c18206c3f899ced8425264a
Volodymyr Zontov <volodymyr.z@cybord.ai>
2024-10-06 14:01:15 +0200

    lesson2

---
+++ lesson2.py
@@ -0,0 +1,18 @@
+number = 12.5
+result = number * number
+result_new_processing = result
+_result = "asd"
+
+_import_1_ = 123
+
+BASE_DIR = "PythonIntro"
+
+__ = 111
+
+print(result_new_processing, _result, __)
+#############################################
+# _ = 10
+# __ = _ * _
+# ___ = __
+# __ = 111
+# print(___, __, _)
