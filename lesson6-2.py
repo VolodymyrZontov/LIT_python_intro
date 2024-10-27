ada611ab87a5babe592bc6fcfff35be7ed1e7c2a
Volodymyr Zontov <volodymyr.z@cybord.ai>
2024-10-27 18:33:26 +0100

    Lesson 6

--- lesson6-2.py
+++ lesson6-2.py
@@ -5,37 +5,41 @@
 
 # Обробку помилок
 
+########################################
+
 # value = 10
 #
 # while value > 0:
 #     print(value)
 #     value -= 1
-# print("Done")
-
-#####################################################
+#
+# print("done")
+########################################
 
 # value = 10
 #
 # while True:
 #     print(value)
 #     value -= 1
-#     if value <= 0:
+#     if value <= -100:
 #         break
 #
 # print("Done")
 
-#####################################################
+########################################
 
 # go_loop = True
 #
 # while go_loop:
 #     value = input("Enter a string: ")
 #     print(value)
-#     user_input = input("Do we continue? y/n: ")
+#     user_input = input("Do you want to continue? (y/n): ")
 #     if user_input != "y":
 #         go_loop = False
+#
+# print("Goodby!")
 
-#####################################################
+########################################
 
 # go_loop = True
 #
@@ -45,30 +49,34 @@
 #     try:
 #         value_int = int(value)
 #         result = value_int ** 2
-#         new_result = 1 / result
+#         new_res = 1 / result
+#         print("new_res", new_res)
 #     # except ValueError:
 #     #     result = -1
 #     # except ZeroDivisionError:
 #     #     result = -100
-#     except (ValueError, ZeroDivisionError) :
+#     except (ValueError, ZeroDivisionError):
 #         result = -1000
-#     print(result)
 #
-#     ######################
+#     #############################
 #     # if value.isdigit():
 #     #     value_int = int(value)
 #     #     result = value_int ** 2
 #     # else:
 #     #     result = -1
-#     # print(result)
-#     ######################
+#     #############################
 #
-#     user_input = input("Do we continue? y/n: ")
+#     print(result)
+#
+#
+#     user_input = input("Do you want to continue? (y/n): ")
 #     if user_input != "y":
 #         go_loop = False
+#
+# print("Goodby!")
 
-#####################################################
-from random import randint
-
-rand_value = randint(1, 10)
-print(rand_value)
+########################################
+# from random import randint
+#
+# rand_value = randint(1, 10)
+# print(rand_value)
