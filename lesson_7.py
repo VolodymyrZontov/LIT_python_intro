a38b305adabe1b7f90e8cb9d2a532bc509ebbce1
Volodymyr Zontov <volodymyr.z@cybord.ai>
2025-03-29 13:54:25 +0100

    Lesson 7

--- lesson_7.py
+++ lesson_7.py
@@ -2,97 +2,90 @@ import random
 from tkinter import *
 
 
-class My15:
+class FifteenPuzzle:
     def __init__(self):
-        self.my_15 = self._create_start_position()
-        self.row, self.col = self._get_empty_cell()
+        self.board = self._create_start_position()
+        self._empty_cell = self._get_empty_cell()
 
     def _get_empty_cell(self):
         row = -1
         col = -1
-        for row_index, row_tmp in enumerate(self.my_15):
+        for row_index, row_tmp in enumerate(self.board):
             for col_index, col_tmp in enumerate(row_tmp):
                 if col_tmp == "":
                     return row_index, col_index
         return row, col
 
     def _create_start_position(self):
-        my_15 = []
+        board = []
         all_values = [str(i) for i in range(1, 16)] + [""]
         random.shuffle(all_values)
         for row_number in range(4):
-            my_15.append(all_values[4 * row_number: 4 * (row_number + 1)])
-        return my_15
+            board.append(all_values[4 * row_number: 4 * (row_number + 1)])
+        return board
 
     def __repr__(self):
-        return "\n".join(["\t".join(row) for row in self.my_15])
+        return "\n".join(["\t".join(row) for row in self.board])
 
-    def move_down(self):
-        if self.row == 3:
-            return
-        self.my_15[self.row][self.col], self.my_15[self.row + 1][self.col] = self.my_15[self.row + 1][self.col], \
-                                                                             self.my_15[self.row][self.col]
-        self.row += 1
+    def move(self, direction):
+        row, col = self._empty_cell
+        new_row, new_col = self._empty_cell
 
-    def move_up(self):
-        if self.row == 0:
-            return
-        self.my_15[self.row][self.col], self.my_15[self.row - 1][self.col] = self.my_15[self.row - 1][self.col], \
-                                                                             self.my_15[self.row][self.col]
-        self.row -= 1
+        if direction == "down" and row < 3:
+            new_row = row + 1
+        elif direction == "up" and row > 0:
+            new_row = row - 1
+        elif direction == "right" and col < 3:
+            new_col = col + 1
+        elif direction == "left" and col > 0:
+            new_col = col - 1
 
-    def move_right(self):
-        if self.col == 3:
-            return
-        self.my_15[self.row][self.col], self.my_15[self.row][self.col + 1] = self.my_15[self.row][self.col + 1], \
-                                                                             self.my_15[self.row][self.col]
-        self.col += 1
+        self.board[row][col], self.board[new_row][new_col] = self.board[new_row][new_col], self.board[row][col]
 
-    def move_left(self):
-        if self.col == 0:
-            return
-        self.my_15[self.row][self.col], self.my_15[self.row][self.col - 1] = self.my_15[self.row][self.col - 1], \
-                                                                             self.my_15[self.row][self.col]
-        self.col -= 1
+        self._empty_cell = new_row, new_col
 
 
-def draw_15_table(my_15):
-    for row_index, row in enumerate(my_15.my_15):
-        for col_index, col in enumerate(row):
-            label = Entry(root, width=2, fg='white', bg='black', font=('Arial', 50, 'bold'), justify='center')
-            label.config(highlightbackground="black")
-            label.grid(row=row_index, column=col_index)
-            label.insert(END, col)
+class FifteenPuzzleUI:
+    def __init__(self):
+        self.game = FifteenPuzzle()
+        self._root = Tk()
+        self._root.title("15")
+        self._root.geometry("275x280")
+        self._root.configure(background='black')
+        self._root.bind("<Left>", self._left)
+        self._root.bind("<Right>", self._right)
+        self._root.bind("<Up>", self._up)
+        self._root.bind("<Down>", self._down)
+        self._set_ui()
+
+    def _set_ui(self):
+        for row_index, row in enumerate(self.game.board):
+            for col_index, col in enumerate(row):
+                label = Entry(self._root, width=2, fg='white', bg='black', font=('Arial', 50, 'bold'), justify='center')
+                label.config(highlightbackground="black")
+                label.grid(row=row_index, column=col_index)
+                label.insert(END, col)
+
+    def _move(self, direction):
+        self.game.move(direction)
+        self._set_ui()
+
+    def _left(self, event):
+        self._move("left")
+
+    def _right(self, event):
+        self._move("right")
+
+    def _up(self, event):
+        self._move("up")
+
+    def _down(self, event):
+        self._move("down")
+
+    def run(self):
+        self._root.mainloop()
 
 
-def left(event):
-    my_15.move_left()
-    draw_15_table(my_15)
-
-
-def right(event):
-    my_15.move_right()
-    draw_15_table(my_15)
-
-
-def up(event):
-    my_15.move_up()
-    draw_15_table(my_15)
-
-
-def down(event):
-    my_15.move_down()
-    draw_15_table(my_15)
-
-
-my_15 = My15()
-root = Tk()
-root.title("15")
-root.geometry("275x280")
-root.configure(background='black')
-draw_15_table(my_15)
-root.bind("<Left>", left)
-root.bind("<Right>", right)
-root.bind("<Up>", up)
-root.bind("<Down>", down)
-root.mainloop()
+if __name__ == "__main__":
+    fifteen = FifteenPuzzleUI()
+    fifteen.run()
